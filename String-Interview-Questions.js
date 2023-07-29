bc11e4339c22a9217ced05b1271ecf5d450e2adb
ManaliKatkar <manaliukatkar@gmail.com>
2023-07-29 13:12:17 +0530

    Remove a first character from String

--- String-Interview-Questions.js
+++ String-Interview-Questions.js
@@ -4,4 +4,13 @@ const removeAllChar = (str, char) => {
     return str.split("").filter(c => c !== char).join("");
 }
 
-console.log(removeAllChar('Hello World','l'));
+// console.log(removeAllChar('Hello World','l'));
+
+
+// How do you remove a first character from String?
+
+const removeFirstChar = (str, char) => {
+    return str.replace(char,"");
+}
+
+console.log(removeFirstChar('Hello World','l'));
